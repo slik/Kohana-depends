a7688331b522bc196a3d30222bc77cb1398de77e
Gun.io Whitespace Robot <contact@gun.io>
2011-12-17 05:25:33 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- classes/depends.php
+++ classes/depends.php
@@ -3,25 +3,25 @@
 class Depends
 {
 	protected static $paths = array(MODPATH);
-	
+
 	protected static $modules = array();
-	
+
 	public static function init(array $conf = NULL)
 	{
 		self::$modules = Kohana::modules();
-		
+
 		if($conf === NULL) return;
-		
+
 		if(isset($conf['paths']))
 		{
 			self::$paths = array_merge(self::$paths, $conf['paths']);
 		}
 	}
-	
+
 	public static function on($name)
 	{
 		if(isset(self::$modules[$name])) return;
-		
+
 		foreach(self::$paths as $path)
 		{
 			if(is_dir($path.$name))
